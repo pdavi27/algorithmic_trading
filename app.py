6e0ca939822e7b64d63f3e9998b1dec0403275d5
Parker <pdavi27@gmail.com>
2023-10-02 19:02:06 -0400

    modified:   app.py 	modified:   templates/results.html

--- app.py
+++ app.py
@@ -2,33 +2,24 @@ from flask import Flask, render_template, request
 import yfinance as yf
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import DatetimeAxis
 from bokeh.models import NumeralTickFormatter
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb+srv://admin:Trader!1@cluster0.i8va7ol.mongodb.net/?retryWrites=true&w=majority')
+uri = "mongodb+srv://admin:Trader01D@testingcluster.45vrcox.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['stock_data']
 collection = db['stock_prices']
 
 @app.route('/')
 def index():
-    # Gather data from Yahoo Finance API
-    ticker = 'AAPL'  # Apple Inc.
-    data = yf.Ticker(ticker)
-    df = data.history(period="1y")
-
-    # Create a Bokeh plot for the ticker
-    p = figure(title=f'{ticker} Stock Price', x_axis_label='Date', y_axis_label='Price', x_axis_type='datetime')
-    p.line(df.index, df['Close'], legend_label='Close', line_width=2)
-    # Format y-axis labels as currency
-    p.yaxis.formatter = NumeralTickFormatter(format='$0,0.00')
-
-    script, div = components(p)
-
-    return render_template('index.html', script=script, div=div)
+    return render_template('index.html')
 
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
@@ -64,7 +55,6 @@ def analyze():
                 # If data already exists, you can choose to skip or update it as needed
                 print('Reading data that already exists')
                 df = pd.DataFrame({'Close': existing_data['close_price']}, index=existing_data['date'])
-                pass
             
             # Create a Bokeh plot for the ticker as before
             p = figure(title=f'{ticker} Stock Price', x_axis_label='Date', y_axis_label='Price', x_axis_type='datetime')
@@ -76,12 +66,44 @@ def analyze():
             script, div = components(p)
             bokeh_plots.append((script, div))
         
-
-        # Render a template with the results and Bokeh plots
-        return render_template('results.html', bokeh_plots=bokeh_plots)
+        # Calculate similarity between tickers and retrieve the 5 most similar ones
+        similar_tickers = find_similar_tickers(tickers, selected_period)
+        
+        # Render a template with the results, Bokeh plots, and similar tickers
+        return render_template('results.html', bokeh_plots=bokeh_plots, similar_tickers=similar_tickers)
 
     # Handle cases where the request method is not POST (e.g., GET request)
     return render_template('index.html')
+
+def find_similar_tickers(selected_tickers, selected_period):
+    # Retrieve data from MongoDB for selected tickers
+    ticker_data = []
+    for ticker in selected_tickers:
+        existing_data = collection.find_one({'ticker': ticker, 'period': selected_period})
+        if existing_data:
+            ticker_data.append(existing_data['close_price'])
+
+    if len(ticker_data) < 2:
+        print("Not enough data")
+        return []  # Not enough data to calculate similarity
+    
+    # Convert ticker_data to a list of strings
+    ticker_data_strings = [" ".join(map(str, data)) for data in ticker_data]
+    
+    # Calculate similarity between tickers using cosine similarity
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(ticker_data_strings)
+    similarity_matrix = cosine_similarity(X)
+    
+    # Get the indices of the most similar tickers (excluding the input tickers)
+    similar_indices = []
+    for i in range(len(similarity_matrix)):
+        similar_indices.extend(list(reversed(similarity_matrix[i].argsort()))[1:6])  # Exclude the current ticker
+    
+    # Get the names of the most similar tickers
+    similar_tickers = list(set([selected_tickers[i] for i in similar_indices]))
+    
+    return(similar_tickers)
 
 if __name__ == '__main__':
     app.run(debug=True)
