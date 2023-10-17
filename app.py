952f189635dfa82ee8ab938b511c4615e5f884b4
Parker <pdavi27@gmail.com>
2023-10-17 10:20:36 -0400

    modified:   app.py 	new file:   package-lock.json 	new file:   package.json

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ import yfinance as yf
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import NumeralTickFormatter
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pandas as pd
 from fastdtw import fastdtw
 import numpy as np
+import faiss
+from scipy.spatial.distance import euclidean
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://admin:Trader01D@testingcluster.45vrcox.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://admin2:Trades123@testingcluster.45vrcox.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(uri, server_api=ServerApi('1'))
+
 db = client['stock_data']
 collection = db['stock_prices']
 
@@ -54,6 +57,7 @@ def analyze():
             else:
                 # If data already exists, you can choose to skip or update it as needed
                 print('Reading data that already exists')
+                # add code here to add new data to existing
                 df = pd.DataFrame({'Close': existing_data['close_price']}, index=existing_data['date'])
             
             # Create a Bokeh plot for the ticker as before
@@ -75,40 +79,83 @@ def analyze():
     # Handle cases where the request method is not POST (e.g., GET request)
     return render_template('index.html')
 
-def find_similar_tickers(entered_tickers, selected_period):
-    # Retrieve data from MongoDB for all tickers in the database for the selected period
-    all_tickers_data = collection.find({'period': selected_period})
+def clean_data(data):
+    # Removing NaN or infinite values by mean imputation
+    clean_data = np.where(np.isnan(data) | np.isinf(data), np.nan, data)
+    nan_indices = np.where(np.isnan(clean_data))
+    
+    for ind in nan_indices:
+        if ind != 0 and ind != len(clean_data)-1:  # Avoid endpoints
+            clean_data[ind] = (clean_data[ind-1] + clean_data[ind+1]) / 2
+        elif ind == 0:
+            clean_data[ind] = clean_data[ind+1]
+        else:
+            clean_data[ind] = clean_data[ind-1]
+    
+    return clean_data
 
-    # Extract ticker names and close price data from the database
+def preprocess_ticker_data(ticker_data, ticker_dates):
+    # Determine the length of the longest sequence
+    max_length = max(len(sublist) for sublist in ticker_data)
+    
+    # Pad shorter sequences with np.nan
+    for sublist in ticker_data:
+        while len(sublist) < max_length:
+            sublist.append(np.nan)
+    
+    return ticker_data
+
+def find_similar_tickers(entered_tickers, selected_period):
+    all_tickers_data_cursor = collection.find({'period': selected_period})
+    all_tickers_data_list = list(all_tickers_data_cursor)
+
     ticker_names = []
     ticker_data = []
-    for data in all_tickers_data:
-        ticker_names.append(data['ticker'])
-        ticker_data.append(data['close_price'])
+
+    for data in all_tickers_data_list:
+        if data['close_price']:
+            ticker_names.append(data['ticker'])
+            ticker_data.append(data['close_price'])
+
+    print(f"Ticker names from DB: {ticker_names}")  # Debugging line
 
     if len(ticker_data) < 2:
-        return []  # Not enough data in the database to calculate similarity
+        return []
 
-    # Initialize a list to store similar tickers for each entered ticker
+    ticker_data = [clean_data(np.array(data)) for data in ticker_data]
+    ticker_data = [data for data in ticker_data if len(data) > 0]
+
+    index = faiss.IndexFlatL2(len(ticker_data[0]))
+    index.add(np.array(ticker_data).astype(np.float32))
+
     similar_tickers = []
 
     for entered_ticker in entered_tickers:
-        # Find the index of the entered ticker in the list of ticker names
+        if entered_ticker not in ticker_names:
+            print(f"Ticker not found: {entered_ticker}")  # Debugging line
+            continue
+
         entered_ticker_index = ticker_names.index(entered_ticker)
 
-        # Calculate DTW distance between the entered ticker and all other tickers
-        distances = []
-        for i, data in enumerate(ticker_data):
-            if i != entered_ticker_index:
-                # Use fastdtw to calculate DTW distance
-                distance, _ = fastdtw(np.array(ticker_data[entered_ticker_index]), np.array(data))
-                distances.append((ticker_names[i], distance))
+        try:
+            _, I = index.search(np.array([ticker_data[entered_ticker_index]]).astype(np.float32), 6)
+        except IndexError:
+            print(f"IndexError encountered for {entered_ticker}")  # Debugging line
+            continue
 
-        # Sort by DTW distance and get the top 5 most similar tickers
-        distances.sort(key=lambda x: x[1])
-        similar_tickers.append([ticker[0] for ticker in distances[:5]])
+        similar_tickers_for_this = set()
+        for i in I[0]:
+            if i != entered_ticker_index and i < len(ticker_names):
+                similar_tickers_for_this.add(ticker_names[i])
+
+                if len(similar_tickers_for_this) >= 5:
+                    break
+
+        similar_tickers.append(list(similar_tickers_for_this))
 
     return similar_tickers
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
